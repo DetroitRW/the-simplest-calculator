28cdd138f0ed184a37729ddc09dd4edb2f84fca2
Ilya Brant <detroitib31@gmail.com>
2022-11-23 07:03:22 +0600

    Написал цикл с разными вариантами

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,3 +3,50 @@ import java.util.Scanner
 fun main(args: Array<String>) {
     val reader = Scanner(System.`in`)
 
+    while (true) {
+        print("Введите знак операции: ")
+        val z = reader.next()
+
+        if (z == "0") {
+            break
+        } else if (z == "+") {
+            print("Введите число: ")
+            val a = reader.nextInt()
+
+            print("Введите число: ")
+            val b = reader.nextInt()
+
+            println(a + b)
+        } else if (z == "-") {
+            print("Введите число: ")
+            val a = reader.nextInt()
+
+            print("Введите число: ")
+            val b = reader.nextInt()
+
+            println(a - b)
+        } else if (z == "*") {
+            print("Введите число: ")
+            val a = reader.nextInt()
+
+            print("Введите число: ")
+            val b = reader.nextInt()
+
+            println(a * b)
+        } else if (z == "/") {
+            print("Введите число: ")
+            val a = reader.nextInt()
+
+            print("Введите число: ")
+            val b = reader.nextInt()
+
+            if (b == 0) {
+                println("Делить на ноль нельзя!")
+            } else {
+                println(a / b)
+            }
+        } else {
+            println("Неверный знак операции, знак может быть +, -, *, /")
+        }
+    }
+}
