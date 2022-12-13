886d7f22c0228aa159d3706599f9aa6e2b408b1e
Ilya Brant <detroitib31@gmail.com>
2022-12-13 08:36:05 +0600

    Добавил функции и поменял if else на when

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,33 +5,13 @@ fun main(args: Array<String>) {
     while (true) {
         val z = promtString("Введите знак операции: ")
 
-        if (z == "0") {
-            break
-        } else if (z == "+") {
-            val a = promtDuoble("Введите число: ")
-            val b = promtDuoble("Введите число: ")
-            println("$a + $b = ${a + b}")
-        } else if (z == "-") {
-            val a = promtDuoble("Введите число: ")
-            val b = promtDuoble("Введите число: ")
-
-            println("$a - $b = ${a - b}")
-        } else if (z == "*") {
-            val a = promtDuoble("Введите число: ")
-            val b = promtDuoble("Введите число: ")
-
-            println("$a * $b = ${a * b}")
-        } else if (z == "/") {
-            val a = promtDuoble("Введите число: ")
-            val b = promtDuoble("Введите число: ")
-
-            if (b - 0 < 0.0001) {
-                println("Делить на ноль нельзя!")
-            } else {
-                println("$a / $b = ${a / b}")
-            }
-        } else {
-            println("Неверный знак операции, знак может быть +, -, *, /")
+        when (z) {
+            "0" -> break
+            "+" -> plus()
+            "-" -> minus()
+            "*" -> multiply()
+            "/" -> divide()
+             else -> println("Неверный знак операции, знак может быть +, -, *, /")
         }
     }
 }
@@ -46,6 +26,34 @@ fun promtDuoble(text: String): Double {
     val reader = Scanner(System.`in`)
     print(text)
     return reader.nextDouble()
+}
+
+fun plus() {
+    val a = promtDuoble("Введите число: ")
+    val b = promtDuoble("Введите число: ")
+    println("$a + $b = ${a + b}")
+}
+fun minus() {
+    val a = promtDuoble("Введите число: ")
+    val b = promtDuoble("Введите число: ")
+    println("$a - $b = ${a - b}")
+}
+
+fun multiply() {
+    val a = promtDuoble("Введите число: ")
+    val b = promtDuoble("Введите число: ")
+    println("$a * $b = ${a * b}")
+}
+
+fun divide() {
+    val a = promtDuoble("Введите число: ")
+    val b = promtDuoble("Введите число: ")
+
+    if (b - 0 < 0.0001) {
+        println("Делить на ноль нельзя!")
+    } else {
+        println("$a / $b = ${a / b}")
+    }
 }
 
 
