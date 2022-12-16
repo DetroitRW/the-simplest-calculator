39d1854ae2e2190f58309cd3672026f5b9fe078f
Ilya Brant <detroitib31@gmail.com>
2022-12-16 15:14:21 +0600

    Создал класс калькулятор

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,13 +4,14 @@ fun main(args: Array<String>) {
 
     while (true) {
         val z = promtString("Введите знак операции: ")
+        val calculator = Calculator()
 
         when (z) {
             "0" -> break
-            "+" -> plus()
-            "-" -> minus()
-            "*" -> multiply()
-            "/" -> divide()
+            "+" -> calculator.plus()
+            "-" -> calculator.minus()
+            "*" -> calculator.multiply()
+            "/" -> calculator.divide()
              else -> println("Неверный знак операции, знак может быть +, -, *, /")
         }
     }
@@ -28,31 +29,34 @@ fun promtDuoble(text: String): Double {
     return reader.nextDouble()
 }
 
-fun plus() {
-    val a = promtDuoble("Введите число: ")
-    val b = promtDuoble("Введите число: ")
-    println("$a + $b = ${a + b}")
-}
-fun minus() {
-    val a = promtDuoble("Введите число: ")
-    val b = promtDuoble("Введите число: ")
-    println("$a - $b = ${a - b}")
-}
+class Calculator {
+    fun plus() {
+        val a = promtDuoble("Введите число: ")
+        val b = promtDuoble("Введите число: ")
+        println("$a + $b = ${a + b}")
+    }
 
-fun multiply() {
-    val a = promtDuoble("Введите число: ")
-    val b = promtDuoble("Введите число: ")
-    println("$a * $b = ${a * b}")
-}
+    fun minus() {
+        val a = promtDuoble("Введите число: ")
+        val b = promtDuoble("Введите число: ")
+        println("$a - $b = ${a - b}")
+    }
 
-fun divide() {
-    val a = promtDuoble("Введите число: ")
-    val b = promtDuoble("Введите число: ")
+    fun multiply() {
+        val a = promtDuoble("Введите число: ")
+        val b = promtDuoble("Введите число: ")
+        println("$a * $b = ${a * b}")
+    }
 
-    if (b - 0 < 0.0001) {
-        println("Делить на ноль нельзя!")
-    } else {
-        println("$a / $b = ${a / b}")
+    fun divide() {
+        val a = promtDuoble("Введите число: ")
+        val b = promtDuoble("Введите число: ")
+
+        if (b - 0 < 0.0001) {
+            println("Делить на ноль нельзя!")
+        } else {
+            println("$a / $b = ${a / b}")
+        }
     }
 }
 
